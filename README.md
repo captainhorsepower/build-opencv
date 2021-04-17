d0b4b1c842d872688b9ec4c27026ac8fbf7478a6
Artsiom Varabei <35666863+captainhorsepower@users.noreply.github.com>
2021-04-17 23:10:03 +0300

    build dirctly to venv; add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Build opencv, locally
+
+This is my config to build opencv. Sick and tired of repeating the 
+same thing each time I need it, so now it's version controlled!
+
+soon will describe what I've done, how and why
---
+++ do-cmake.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+
+mkdir -p build
+
+cd build
+
+# install directly to virtual env to make life simplier
+cmake -GNinja \
+    -D CMAKE_INSTALL_PREFIX="$HOME/.virtualenvs/3D-reconstruction" \
+    -D CMAKE_BUILD_TYPE='Release' \
+    -D OPENCV_ENABLE_NONFREE='ON' \
+    "../opencv"
+    # -D OPENCV_EXTRA_MODULES_PATH='../extra-modules' \
